3dc146e3281a5099ff0052f6d8afe3b794403dbe
uspehgp <uspehgp@gmail.com>
2021-06-18 17:38:50 +0300

    add routing and some new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { RoomsComponent } from './admin/rooms/rooms.component';
-import { UsersComponent } from './admin/users/users.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {RoomsComponent} from './admin/rooms/rooms.component';
+import {UsersComponent} from './admin/users/users.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {RouterModule, Routes} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: 'admin/users', component: UsersComponent},
+  {path: 'admin/rooms', component: RoomsComponent},
+  {path:'', component: CalendarComponent},
+  {path:'404', component: PageNotFoundComponent},
+  {path:'**', redirectTo: '/404'}
+];
 
 @NgModule({
   declarations: [
@@ -13,12 +23,15 @@ import { CalendarComponent } from './calendar/calendar.component';
     MenuComponent,
     RoomsComponent,
     UsersComponent,
-    CalendarComponent
+    CalendarComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
